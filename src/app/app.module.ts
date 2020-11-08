5489e42c03a6745ee2f69be83c032de1710b3f3d
Emanuel Antonio <Emanuel Antonio@DESKTOP-10ULSAH.(none)>
2020-11-08 16:52:48 -0600

    authapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+import { AuthModule } from '@auth0/auth0-angular';
+import { CallbackComponent } from './components/callback/callback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-s8sqh-5g.us.auth0.com',
+      clientId: 'RZost2jbL5EV8mO3y7v6iGYCTSX0PIx0'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
